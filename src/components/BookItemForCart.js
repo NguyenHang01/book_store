e7e3c2c820da5c3c1b102e04801628896ac802c9
Hangnta <nguyenanhhang1999@gmail.com>
2020-05-08 01:08:28 +0700

    [Add] Add function for button delete on BookItemForCart

--- src/components/BookItemForCart.js
+++ src/components/BookItemForCart.js
@@ -9,14 +9,15 @@ class BooktItemForCart extends Component {
     super(props);
     this.state = {
       id_book: this.props.id_book,
-      book:{},
+      book: {},
+      quantity: this.props.id_book.so_luong,
+      isDelete: 0
     };
   }
 
   getBookById = async id_book => {
     let data = {};
     await db
-      // .collection("sach")
       .doc(`sach/${id_book.id}`)
       .get()
       .then(snapshot => {
@@ -26,14 +27,14 @@ class BooktItemForCart extends Component {
       .catch(err => {
         console.log("Error getting documents", err);
       });
-    data.quantity = id_book.so_luong; //so luong sach co id=book.id trong gio hang
+    // data.quantity = id_book.so_luong; //so luong sach co id=book.id trong gio hang
     return data;
   };
 
-  async componentDidMount(){
-    const {id_book} = this.state;
-    const book= await this.getBookById(id_book);
-    this.setState({book:book});
+  async componentDidMount() {
+    const { id_book } = this.state;
+    const book = await this.getBookById(id_book);
+    this.setState({ book: book });
   }
   openNotificationWithIcon = (type, message) => {
     notification[type]({
@@ -41,101 +42,134 @@ class BooktItemForCart extends Component {
     });
   };
 
-  changeQuantity = e => {
-    const { book } = this.state;
-    let quantity=book.quantity;
+  changeQuantity = async e => {
+    let { quantity, book } = this.state;
     switch (e.target.id) {
       case "tang":
         if (quantity === book.so_luong)
+          //Neu so sach trong gio = so sach trong kho
           this.openNotificationWithIcon(
             "warning",
             "Không thể mua quá số lượng trong kho"
           );
-        else  quantity= quantity + 1 ;
+        else quantity = quantity + 1;
         break;
       case "giam":
-        if (quantity > 1)  quantity= quantity - 1;
+        if (quantity > 1) quantity = quantity - 1; //So sach trong gio khong the bang 0
         break;
     }
-    book.quantity=quantity;
-    this.setState({book: book});
+    await this.UpdateCart(quantity);
+    this.setState({ quantity: quantity });
   };
 
-  UpdateCart=()=>{
+  UpdateCart = async quantity => {
+    const uid = firebase.auth().currentUser.uid;
+    const id_book = this.state.book.id;
+    await db
+      .doc(`gio_hang/${uid}/sach/${id_book}`)
+      .update({ so_luong: quantity })
+      .then(() => {
+        console.log("Update success!!");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
+  DeleteBookOnCart= async() =>{
+    const uid = firebase.auth().currentUser.uid;
+    const id_book = this.state.book.id;
+    await db
+      .doc(`gio_hang/${uid}/sach/${id_book}`)
+      .delete()
+      .then(()=>{
+        console.log("Delete success");
+      })
+      .catch(err=>{
+        console.log(err);
+      })
   }
 
-  confirm=(e)=>{
-
-  }
+  confirm =async e => {
+    await this.DeleteBookOnCart();
+    this.setState({ isDelete: 1 });
+  };
 
   render() {
-    const { anh, ten, gia_bia, chiet_khau, id, quantity } = this.state.book;
+    const { anh, ten, gia_bia, chiet_khau, id } = this.state.book;
+    const { quantity, isDelete } = this.state;
     return (
-      <div
-        style={{ marginLeft: 10 }}
-        className={`gx-product-item gx-product-horizontal`}
-      >
-        <div style={{ width: 160, height: 210 }} className="gx-product-image">
-          <img
-            style={{
-              height: 200,
-              width: 170,
-              marginRight: 5,
-              marginLeft: 10,
-              marginBottom: 3
-            }}
-            alt="book"
-            src={anh}
-          />
-        </div>
-        <Link to={`/book/${id}`}>
-          <div className="gx-product-body">
-            <div style={{ height: 60, width: 145 }}>
-              <h4>{ten}</h4>
-            </div>
-            <div className="ant-row-flex">
-              <h4>{Math.floor(gia_bia * (1 - chiet_khau / 100))}đ </h4>
-              <h5 className="gx-text-muted gx-px-2">
-                <del>{gia_bia}đ</del>
-              </h5>
-              <h5 className="gx-text-success">{chiet_khau}% off</h5>
-            </div>
-          </div>
-        </Link>
-        <div>
-          <div style={{ float: "inline-start" }}>
-            <Input.Group compact>
-              <Button
-                id="tang"
-                style={{ width: "18%" }}
-                onClick={this.changeQuantity}
-              >
-                +
-              </Button>
-              <Input style={{ width: "14%" }} value={quantity} />
-              <Button
-                id="giam"
-                style={{ width: "18%" }}
-                onClick={this.changeQuantity}
-              >
-                -
-              </Button>
-            </Input.Group>
-          </div>
-          <div style={{ float: "inline-start" }}>
-            <Popconfirm
-              title="Bạn có muốn xóa sản phẩm này ra khỏi giỏ hàng?"
-              onConfirm={this.confirm}
-              okText="Yes"
-              cancelText="No"
+      <div>
+        {isDelete ? null : (
+          <div
+            style={{ marginLeft: 10 }}
+            className={`gx-product-item gx-product-horizontal`}
+          >
+            <div
+              style={{ width: 160, height: 210 }}
+              className="gx-product-image"
             >
-              <Button type="primary" danger >
-                Xóa
-              </Button>
-            </Popconfirm>
+              <img
+                style={{
+                  height: 200,
+                  width: 170,
+                  marginRight: 5,
+                  marginLeft: 10,
+                  marginBottom: 3
+                }}
+                alt="book"
+                src={anh}
+              />
+            </div>
+            <Link to={`/book/${id}`}>
+              <div className="gx-product-body">
+                <div style={{ height: 60, width: 145 }}>
+                  <h4>{ten}</h4>
+                </div>
+                <div className="ant-row-flex">
+                  <h4>{Math.floor(gia_bia * (1 - chiet_khau / 100))}đ </h4>
+                  <h5 className="gx-text-muted gx-px-2">
+                    <del>{gia_bia}đ</del>
+                  </h5>
+                  <h5 className="gx-text-success">{chiet_khau}% off</h5>
+                </div>
+              </div>
+            </Link>
+            <div>
+              <div style={{ float: "inline-start" }}>
+                <Input.Group compact>
+                  <Button
+                    id="tang"
+                    style={{ width: "18%" }}
+                    onClick={this.changeQuantity}
+                  >
+                    +
+                  </Button>
+                  <Input style={{ width: "14%" }} value={quantity} />
+                  <Button
+                    id="giam"
+                    style={{ width: "18%" }}
+                    onClick={this.changeQuantity}
+                  >
+                    -
+                  </Button>
+                </Input.Group>
+              </div>
+              <div style={{ float: "inline-start" }}>
+                <Popconfirm
+                  title="Bạn có muốn xóa sản phẩm này ra khỏi giỏ hàng?"
+                  onConfirm={this.confirm}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button type="primary" danger>
+                    Xóa
+                  </Button>
+                </Popconfirm>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
